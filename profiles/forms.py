3dacc5c4286ee7b882b5080533834564d7b8be9e
luigiay1123 <wemixmir801@gmail.com>
2026-05-17 08:31:44 +0800

    47. Adding borders for first_name, last_name, and username

--- profiles/forms.py
+++ profiles/forms.py
@@ -6,6 +6,11 @@ class UserProfileForm(forms.ModelForm):
   class Meta:
     model = User
     fields = ['first_name', 'last_name', 'username']
+    widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'w-full border border-gray-300 rounded px-3 py-2'}),
+            'last_name': forms.TextInput(attrs={'class': 'w-full border border-gray-300 rounded px-3 py-2'}),
+            'username': forms.TextInput(attrs={'class': 'w-full border border-gray-300 rounded px-3 py-2'}),
+        }
 
 class ProfileImageForm(forms.ModelForm):
   class Meta:
